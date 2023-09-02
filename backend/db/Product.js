8031abbca96c803977a48ee7464ba47b32f254ab
sac6inarya <sachinkarya333@gmail.com>
2023-09-02 16:42:20 +0530

    logic for sending image with other product details

--- backend/db/Product.js
+++ backend/db/Product.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema= new mongoose.Schema({
-    name:String,
-    price:String,
-    category:String,
-    userId:String,
-    company:String
-})
-module.exports = mongoose.model('products',productSchema)
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: String,
+  image: String,
+  category: String,
+  userId: String,
+  company: String,
+});
+module.exports = mongoose.model("products", productSchema);
--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 const AddProduct = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
+  const [image, setImage] = useState("");
   const [category, setCategory] = useState("");
   const [company, setCompany] = useState("");
   const [error, setError] = useState(null);
@@ -14,22 +15,23 @@ const AddProduct = () => {
   const addProduct = async (e) => {
     try {
       e.preventDefault();
-      await validation.validate({ name, price, category, company });
+      await validation.validate({ name, price, category, company, image });
 
-      console.log(name, price, category, company);
+      console.log(name, price, category, company, image);
       const userId = JSON.parse(localStorage.getItem("user"))._id;
       let result = await fetch("http://localhost:5000/add-product", {
         method: "post",
-        body: JSON.stringify({ name, price, category, company, userId }),
+        body: JSON.stringify({ name, price, category, company, userId, image }),
         headers: { "Content-Type": "application/json" },
       });
       result = await result.json();
       navigate("/");
       console.log(result);
     } catch (error) {
-      error instanceof Yup.ValidationError
-        ? setError("Please enter valid details")
-        : setError("Product could not be added");
+      if (error instanceof Yup.ValidationError) {
+        setError("Please enter valid details");
+      }
+      setError("Product could not be added");
     }
   };
 
@@ -38,6 +40,7 @@ const AddProduct = () => {
     price: Yup.number().required("Price is required"),
     category: Yup.string().required("Category is required"),
     company: Yup.string().required("Company is required"),
+    image: Yup.string().required("Image link is required"),
   });
 
   return (
@@ -67,9 +70,22 @@ const AddProduct = () => {
               type="number"
               className="mt-3"
               placeholder="Enter price"
-              value={price}
+              value={price === 0 ? "" : price}
               onChange={(e) => {
                 setPrice(e.target.value);
+                setError(null);
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              value={image}
+              className="mt-3"
+              placeholder="Enter image link"
+              name="image"
+              onChange={(e) => {
+                setImage(e.target.value);
                 setError(null);
               }}
             />
